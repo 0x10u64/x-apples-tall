5aa5892933903ef0aaf9ba822755d3c40698a8b0
forgor0x10 <burdygindanil@proton.me>
2026-08-24 18:49:18 +0500

    feat: update result subtext

--- public/js/index.js
+++ public/js/index.js
@@ -51,7 +51,7 @@ async function handleFile(file) {
             const apples = Math.ceil(height / width - 0.5);
             const plural = apples > 1 ? "s" : "";
             els.result.textContent = `${apples} apple${plural} tall`;
-            els.resultSubtext.textContent = "x-apples-tall.0x10u64.vercel.app";
+            els.resultSubtext.textContent = "x-apples-tall.ext.io";
         });
     });
 }
